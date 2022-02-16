b112b6aaa7fed49ab5ff6a570a5715ffea578a29
Thảo Hà <thaoanhhaa1@gmail.com>
2022-02-16 21:29:03 +0700

    Login Page

---
+++ JS/AuthenticatorForm.js
@@ -0,0 +1,16 @@
+const showPasswordBtn = $('.form-group__item__text');
+
+// * Start Handler Show/Hidden Password Form
+
+showPasswordBtn.click(function() {
+    $(this).toggleClass('show');
+
+    const isShow = $(this).hasClass('show');
+    const inputElement = $(this).parent().children('input');
+
+    this.innerHTML = isShow && 'Hide' || 'Show';
+
+    inputElement[0].type = isShow && 'text' || 'password';
+})
+
+// * End Handler Show/Hidden Password Form
