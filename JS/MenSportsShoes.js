d0ffd6497677b2bd9bd8155ed85bd63515ae24fc
Thao Ha <thaoanhhaa1@gmail.com>
2022-04-12 16:13:18 +0700

    MenSportsShoes Filter

--- JS/MenSportsShoes.js
+++ JS/MenSportsShoes.js
@@ -1,5 +1,9 @@
 const categoryIcons = $('.category__icon');
 const categoryPrimaryBody = $('.category__primary__body');
+const categoryProductOnSale = $('.category__product__body--on-sale');
+const categoryProductBestSellers = $('.category__product__body--best-sellers');
+const categoryProductNewProducts = $('.category__product__body--new-products');
+const contentProductFilterSortBy = $('.content-product__filter__sort__by');
 
 categoryIcons.click(function() {
     $(this).toggleClass('active');
@@ -129,3 +133,112 @@ const categoryItemColor = $('.category__item__color');
 })
 
 // * End Set BackgroundColor CategoryListColor
+
+// * Start Add Category Product
+
+const getHTMLListProduct = listProduct => {
+    return `
+        <ul class="category__product__list">
+            ${listProduct.map(item => `
+                <li class="category__product__item">
+                    <div class="category__product__img">
+                        <a href="">
+                            <img class="img-cover " src="./IMG/${item.image}" alt="">
+                            <img class="img-cover hover" src="./IMG/${item.imageHover}" alt="">
+                        </a>
+                    </div>
+                    <div class="category__product__info">
+                        <div class="category__product__rating">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h5 class="category__product__title">
+                            <a href="">${item.name}</a>
+                        </h5>
+                        <div class="category__product__price">
+                            <span class="old-price ${item.discount || 'hidden'}">$${item.price.toFixed(2)}</span>
+                            <span class="discount ${item.discount || 'hidden'}">(-${item.discount}%)</span>
+                            ${item.discount && '<br>' || ''}
+                            <span class="price">$${item.discount ? (item.price*(100 - item.discount)/100).toFixed(2) : item.price.toFixed(2)}</span>
+                        </div>
+                    </div>
+                </li>
+            `)}
+        </ul>
+    `;
+}
+
+const listProductOnSale = [{
+    image: 'MenSportsShoes-OnSale-1.jpg',
+    imageHover: 'MenSportsShoes-OnSale-1-hover.jpg',
+    name: 'Slip On Shoes',
+    price: 390.89,
+    discount: 10
+},{
+    image: 'MenSportsShoes-OnSale-2.jpg',
+    imageHover: 'MenSportsShoes-OnSale-2-hover.jpg',
+    name: 'Zeven Thrust Shoes',
+    price: 290,
+    discount: 10
+},{
+    image: 'MenSportsShoes-OnSale-3.jpg',
+    imageHover: 'MenSportsShoes-OnSale-3-hover.jpg',
+    name: 'Aero Power Shoes',
+    price: 390.89,
+    discount: 20
+}];
+
+const listProductBestSellers = [{
+    image: 'MenSportsShoes-BestSellers-1.jpg',
+    imageHover: 'MenSportsShoes-BestSellers-1-hover.jpg',
+    name: 'Asics Women\'s Shoes',
+    price: 56.99
+},{
+    image: 'MenSportsShoes-BestSellers-2.jpg',
+    imageHover: 'MenSportsShoes-BestSellers-2-hover.jpg',
+    name: 'Nivia Tennis Shoes',
+    price: 105.99
+},{
+    image: 'MenSportsShoes-BestSellers-3.jpg',
+    imageHover: 'MenSportsShoes-BestSellers-3-hover.jpg',
+    name: 'Lunar Force Shoes',
+    price: 88.65
+}];
+
+const listProductNewProducts = [{
+    image: 'MenSportsShoes-NewProduct-1.jpg',
+    imageHover: 'MenSportsShoes-NewProduct-1-hover.jpg',
+    name: 'Zeven Thrust Shoes',
+    price: 290.00,
+    discount: 10
+},{
+    image: 'MenSportsShoes-NewProduct-2.jpg',
+    imageHover: 'MenSportsShoes-NewProduct-2-hover.jpg',
+    name: 'Indoor Court Shoes',
+    price: 45.99
+},{
+    image: 'MenSportsShoes-NewProduct-3.jpg',
+    imageHover: 'MenSportsShoes-NewProduct-3-hover.jpg',
+    name: 'Swerve Women\'s Shoes',
+    price: 44.99
+}];
+
+categoryProductOnSale.html(getHTMLListProduct(listProductOnSale));
+categoryProductBestSellers.html(getHTMLListProduct(listProductBestSellers));
+categoryProductNewProducts.html(getHTMLListProduct(listProductNewProducts));
+
+// * End Add Category Product
+
+// * Start Handler Event Click Sort Product Content
+
+contentProductFilterSortBy.click(function(e) {
+    $(this).toggleClass('active');
+    e.stopPropagation();
+});
+
+app.click(() => contentProductFilterSortBy.removeClass('active'));
+
+// * End Handler Event Click Sort Product Content
